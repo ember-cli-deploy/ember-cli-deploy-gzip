59181381137483df962f7a2c9a470f2dcbdc5ab5
Luke Melia <luke@lukemelia.com>
2023-06-04 15:47:25 -0400

    [BREAKING] Update dependencies and node version requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,48 @@
 module.exports = {
   root: true,
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      configFile: false,
+      // your babel options
+      presets: ["@babel/preset-env"],
+    },
   },
-  extends: 'eslint:recommended',
+  plugins: [
+  ],
+  extends: [
+    'eslint:recommended',
+  ],
   env: {
     browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        'index.js',
+        'config/**/*.js',
+        'tests//config/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+        'node/no-unpublished-require': 'off',
+      })
+    }
+  ]
 };
